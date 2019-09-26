adb33ced0025e0c7b8a13fdb18285e071c5e96df
Santosh Marigowda <santosharakere@gmail.com>
2019-09-26 08:56:06 +0100

    1.2.1 On clicking a button, update the age, which in turn should update the UI

---
+++ Notes.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+- Props
+- Ractivity
+	- On clicking a button, update the age, which in turn should update the UI
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,30 @@
 <script>
-	export let name;
-	export let age;
+  export let name;
+  export let age;
+  const handleClick = () => {
+    console.log("button clicked");
+
+    age = Math.floor(Math.random() * 40);
+  };
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
+<style lang="scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+    & a {
+      color: green;
+    }
+  }
+  .container {
+	  & button {
+		  color: blueviolet;
+	  }
+  }
 </style>
 
 <h1>Hello {name}, your age is {age}!</h1>
+<div class="container">
+  <button on:click={handleClick}>Change Age</button>
+</div>
